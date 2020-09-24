e3811994ea4fce673b2c3d8f98d858d232adf3bd
iamirzashowvik <showvikmirza@gmail.com>
2020-09-24 14:45:54 +0600

    asx

--- lib/category.dart
+++ lib/category.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
-
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:share/share.dart';
 import 'package:squiz/qn.dart';
-
-import 'package:squiz/question.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 //FirebaseFirestore firestore = FirebaseFirestore.instance;
+const String testDevice = 'YOUR_DEVICE_ID';
 
 class Category extends StatefulWidget {
   @override
@@ -16,7 +16,9 @@ class Category extends StatefulWidget {
 }
 
 class _CategoryState extends State<Category> {
+  List<Color> coll = [];
   var b;
+  static String bannerUnitId = 'ca-app-pub-8475411739576687/8371671855';
   void link() async {
     b = await Firestore.instance
         .collection("PlaystoreLink")
@@ -25,10 +27,47 @@ class _CategoryState extends State<Category> {
 //    _showDialog();
   }
 
+  static String appId = 'ca-app-pub-8475411739576687~5306524814';
+  static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    testDevices: testDevice != null ? <String>[testDevice] : null,
+    keywords: <String>['foo', 'bar'],
+    contentUrl: 'http://foo.com/bar.html',
+    childDirected: true,
+    nonPersonalizedAds: true,
+  );
+
+  BannerAd _bannerAd;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+      adUnitId: 'ca-app-pub-8475411739576687/8371671855',
+      size: AdSize.banner,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("BannerAd event $event");
+      },
+    );
+  }
+
+//ca-app-pub-8475411739576687/2927773486
   @override
   void initState() {
     link();
     super.initState();
+    FirebaseAdMob.instance
+        .initialize(appId: 'ca-app-pub-8475411739576687~5306524814');
+    _bannerAd = createBannerAd()..load();
+
+    _bannerAd ??= createBannerAd();
+    _bannerAd
+      ..load()
+      ..show();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
   }
 
   @override
@@ -54,54 +93,63 @@ class _CategoryState extends State<Category> {
             ),
           ),
           centerTitle: true,
+          //  gradient: LinearGradient(colors: [Colors.red, Colors.yellow]),
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: 300.h,
-              child: Center(
-                child: TextResponsive('SABI DIRI',
-                    style: GoogleFonts.hanalei(
-                        fontSize: 250.h, color: Colors.black)
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [Colors.red, Colors.yellow])),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 300.h,
+                child: Center(
+                  child: TextResponsive('SABI DIRI',
+                      style: GoogleFonts.hanalei(
+                          fontSize: 250.h, color: Colors.black)
 //                  TextStyle(fontSize: 250.h,
 //
 //                      color: Colors.black),
-                    ),
+                      ),
+                ),
               ),
-            ),
-            StreamBuilder(
-                stream: Firestore.instance.collection('Categories').snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(),
+              StreamBuilder(
+                  stream:
+                      Firestore.instance.collection('Categories').snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    return ListView(
+                      shrinkWrap: true,
+                      children: snapshot.data.documents.map(
+                        (document) {
+                          return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            CountDownTimer(document.get('name'))
+                                        //  Question(
+
+                                        // )
+
+                                        ));
+                              },
+                              child: CategoryX(document.get('name')));
+                        },
+                      ).toList(),
                     );
-                  }
-
-                  return ListView(
-                    shrinkWrap: true,
-                    children: snapshot.data.documents.map(
-                      (document) {
-                        return GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          CountDownTimer(document.get('name'))
-                                      //  Question(
-
-                                      // )
-
-                                      ));
-                            },
-                            child: CategoryX(document.get('name')));
-                      },
-                    ).toList(),
-                  );
-                })
-          ],
+                  })
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xff347F3D),
@@ -147,3 +195,37 @@ class CategoryX extends StatelessWidget {
     );
   }
 }
+
+// MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+//   keywords: <String>['flutterio', 'beautiful apps'],
+//   contentUrl: 'https://flutter.io',
+//   birthday: DateTime.now(),
+//   childDirected: false,
+//   designedForFamilies: false,
+//   gender:
+//       MobileAdGender.male, // or MobileAdGender.female, MobileAdGender.unknown
+//   testDevices: <String>[], // Android emulators are considered test devices
+// );
+
+// BannerAd myBanner = BannerAd(
+//   // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+//   // https://developers.google.com/admob/android/test-ads
+//   // https://developers.google.com/admob/ios/test-ads
+//   adUnitId: 'ca-app-pub-8475411739576687/8371671855',
+//   size: AdSize.smartBanner,
+//   targetingInfo: targetingInfo,
+//   listener: (MobileAdEvent event) {
+//     print("BannerAd event is $event");
+//   },
+// );
+
+// InterstitialAd myInterstitial = InterstitialAd(
+//   // Replace the testAdUnitId with an ad unit id from the AdMob dash.
+//   // https://developers.google.com/admob/android/test-ads
+//   // https://developers.google.com/admob/ios/test-ads
+//   adUnitId: InterstitialAd.testAdUnitId,
+//   targetingInfo: targetingInfo,
+//   listener: (MobileAdEvent event) {
+//     print("InterstitialAd event is $event");
+//   },
+// );
